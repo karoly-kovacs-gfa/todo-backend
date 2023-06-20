42d7ec367bcb472f5df36ad3a21c8b68fc4acfc3
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2023-06-20 10:25:36 +0200

    feat(): update build.gradle with manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.kare.todobackend"
+    }
+}
+
 group = "com.kare"
 version = "0.0.1-SNAPSHOT"
 
